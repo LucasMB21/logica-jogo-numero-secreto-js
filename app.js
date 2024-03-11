8e7caf7413caaffa5e049681a890e7577116ca63
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-11 18:49:18 -0300

    update app.js
    foi incluido condições

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log('Isso ai! você descobriu o número secreto (5)');
+    alert('Isso ai! você descobriu o número secreto (5)');
+} else{
+    alert('Você errou :( ')
 }
