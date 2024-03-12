6e8233b6c808b67c3713e6094f293cfcd7c56779
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-12 13:39:49 -0300

    update app.js
    foi adicionado a função while, caso a pessoa erre o jogo ele vai coninuar perguntando e dando dicas sobre o número secreto até que a pessoa acerte.

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute 
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
-} else{
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
-    } else {
-       alert(`O número secreto é maior que ${chute}`)
-    }
-    
+// enquanto chute não for igual ao n. 5.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
+    } else{
+       if (chute > numeroSecreto) {
+           alert(`O número secreto é menor que ${chute}`);
+       } else {
+          alert(`O número secreto é maior que ${chute}`)
+       } 
+   }
 }
