89fc1e37f1814cc6de2068e2095bcb3b5a7a25a7
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-12 13:01:06 -0300

    update.app.js
    foi adicionado a opção para dizer quando a pessoa errar se o numero secreto é maior ou menor que o número respondido

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai! você descobriu o número secreto (5)');
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
 } else{
-    alert('Você errou :( ')
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+       alert(`O número secreto é maior que ${chute}`)
+    }
+    
 }
