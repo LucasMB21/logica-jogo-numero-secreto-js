c40e2cd7c28b85b9177cea69a25622c2d01ec8a3
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-13 12:47:06 -0300

    update app.js
    agora quando a pessoa acertar o numero vai aparecer quantas tentativas a pessoa utilizou para acertar.

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute 
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 // enquanto chute não for igual ao n. 5.
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else{
        if (chute > numeroSecreto) {
            alert(`O número secreto é menor que ${chute}`);
        } else {
-          alert(`O número secreto é maior que ${chute}`)
+          alert(`O número secreto é maior que ${chute}`);
        } 
+       //tentativas = tentativas +1;
+       tentativas++;
    }
 }
