d20c5a7f86f17e4fc6e22789f8f3d64c510f0e3a
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-14 18:25:59 -0300

    update app.js
    finalizado foi finalizado o projeto e foi acrescentado uma opção que o número muda a cada rodada e pode ser aleartório entre 1 e 5000.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n. secreto .
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
@@ -24,9 +25,3 @@ while (chute != numeroSecreto) {
 
 let palavraTentativa = tentativas > 1 ?  'tentativas' : 'tentativa';
 alert(`Isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-//if (tentativas > 1) {
-//    alert(`Isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-//} else{
-//    alert(`Isso aí! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`); 
-//}
