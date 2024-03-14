f93f5fbc5b7a02faf477719fbf3795a3e82eab67
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-13 23:38:31 -0300

    update app.js
    foi feita a alteração para o número secreto ser um número aleartório entre 1 e 100.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-// enquanto chute não for igual ao n. 5.
+// enquanto chute não for igual ao n. secreto .
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
